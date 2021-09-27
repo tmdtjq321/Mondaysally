d756a8c0626de83c25b0958b419a088a9d553e49
root <root@ip-172-31-12-239.ap-northeast-2.compute.internal>
2021-09-27 17:53:09 +0900

    config change

--- config/database.js
+++ config/database.js
@@ -3,7 +3,12 @@ const {logger} = require('./winston');
 
 // TODO: 본인의 DB 계정 입력
 const pool = mysql.createPool({
-    
+    host: 'mondaysallydatabase.cac7s5ecpj9r.ap-northeast-2.rds.amazonaws.com',
+    user: 'lawofsally',
+    port: '3306',
+    password: 'sally0630!!',
+    database: 'Mondaysally',
+    charset: 'utf8mb4'
 });
 
 module.exports = {
